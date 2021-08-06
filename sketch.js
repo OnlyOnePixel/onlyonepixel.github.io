58d25c219d1caf7bd26acb51cea73edc7492dbe2
Rube Royce Jr <ruberoycejr@protonmail.comi>
2021-08-05 22:58:31 -0400

    updates

--- sketch.js
+++ sketch.js
@@ -1,53 +1,25 @@
-const coordinates = [];
-
-for (let i = 0; i < 32; i++) {
-	for (let j = 0; j < 32; j++) {
-		let p = [i, j];
-
-		coordinates.push(p);
-		// console.log(p);
-	}
-}
-
 var colors = [
-	"#0000db",
-	"#00b6db",
-	"#00db6d",
-	"#ffb600",
-	"#ff926d",
-	"#db0000",
-	"#dbdbdb",
 	"#000000",
+	"#ffffff",
+	"#2E2C9B",
+	"#813338",
+	"#56AC4D",
+	"#EDF171",
+	"#8E3C97",
+	"#75CEC8",
 ];
-
-var colorCoordinatePair = {};
-var a = colors;
-for (let i = 0; i < coordinates.length; i++) {
-	if (a.length > 0) {
-		colorCoordinatePair[coordinates[i]] = a[0];
-		a.shift();
-	} else {
-		a = colors;
-		//console.log("colors reset");
-	}
-	//console.log(a);
-}
-console.log("Done iterating");
-console.log(coordinates.length);
-
-// for (let a = 0; a < coordinates.length; a++) {
-// 	console.log(coordinates[a]);
-// }
-
-// console.log("test", colors);
-
-console.log("color coordinate dictionary", JSON.stringify(colorCoordinatePair));
 
 function setup() {
 	createCanvas(320, 320);
 	background(50);
+	for(let i = 0; i < 32; i++){
+		for(let j = 0; j < 32; j++){
+			let color = colors[~~(Math.random() * colors.length)]
+			fill(color)
+			rect(0 + (i*10), 0 + (j*10), 10, 10);		
+		}
+	}
 }
 function draw() {
-	fill(225);
-	rect(50, 50, 50, 50);
 }
+

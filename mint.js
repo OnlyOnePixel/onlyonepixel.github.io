6d23180ff14d4684c6209ca264a26ce5effa93fa
Rube Royce Jr <ruberoycejr@protonmail.comi>
2021-08-06 00:06:04 -0400

    add web3 connect via MM and basic web3 functionality

--- mint.js
+++ mint.js
@@ -1,13 +1,12 @@
 var PROVIDER, SIGNER, CONTRACT;
 var CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-async function connect(){
+async function mint(){
 	await ethereum.request( {method:"eth_requestAccounts"} );
 	PROVIDER = new ethers.providers.Web3Provider( window.ethereum )
     SIGNER = PROVIDER.getSigner()
     let ABI = await fetch( "assets/abi/OnePixel.json" ).then( e=>e.json() )
     CONTRACT = new ethers.Contract( CONTRACT_ADDRESS, ABI,  SIGNER);
-    let supply = await CONTRACT.totalSupply()
-    document.getElementById("mintCount").innerHTML = "Minted: " + String(supply);
-    document.getElementById("remainingCount").innerHTML = "Remaining: " + String(1024-supply);
+    let amount = parseInt(document.getElementById("InputAmount").value)
+    await CONTRACT.bulkBuy(amount, {value:String(amount * 50000000000000000)})
 }
--- sketch.js
+++ sketch.js
@@ -14,12 +14,15 @@ function setup() {
 	background(50);
 	for(let i = 0; i < 32; i++){
 		for(let j = 0; j < 32; j++){
-			let color = colors[~~(Math.random() * colors.length)]
+			let color = "grey"
 			fill(color)
 			rect(0 + (i*10), 0 + (j*10), 10, 10);		
 		}
 	}
 }
 function draw() {
+	//let color = colors[~~(Math.random() * colors.length)]
+	//fill(color)
+	//rect(0 + (i*10), 0 + (j*10), 10, 10);		
 }
 

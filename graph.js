8039b5fcb65b9d3059f4697c35f995b216e1979c
Rube Royce Jr <ruberoycejr@protonmail.comi>
2021-08-07 14:19:00 -0400

    update api key

--- graph.js
+++ graph.js
@@ -1,7 +1,7 @@
 var colorGrid;
 
 async function getColor() {
-  await fetch("https://gateway.thegraph.com/api/ece478bb52e62e8cec11a1c227ecfc6c/subgraphs/id/0x731b9ebf896fbc520698501721edc31f025e2074-0", {
+  await fetch("https://gateway.thegraph.com/api/c0914300c8224ccf4c24308eaf3fab23/subgraphs/id/0x731b9ebf896fbc520698501721edc31f025e2074-0", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",

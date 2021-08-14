5909345ff4a84f2c8bf5def3aed93d9d569c95b9
RubeRoyceJr <ruberoycejr@protonmail.com>
2021-08-14 00:18:46 -0400

    only display color changer if user has pixel balance

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,11 @@ async function main(){
     if(SIGNER){
         let address = await SIGNER.getAddress()
         document.getElementById("account-name").innerHTML = "Signed in as: <a href=\"https://opensea.io/" + address + "\">" + address.substring(0,7) + "</a>"
-        document.getElementById('colorChanger').style.display = '';
-        await fillDropdown()
-    } else {
+        let pxlBalance = await CONTRACT.balanceOf(address);
+        if(pxlBalance > 0){
+            document.getElementById('colorChanger').style.display = '';
+            await fillDropdown()                
+        }
     }
+
 }
